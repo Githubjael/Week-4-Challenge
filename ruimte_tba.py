2a1a06e1cac69773b70f197a54d97a58361877c7
Githubjael <jaelimamdi@gmail.com>
2023-09-27 11:01:24 +0200

    om dudilijker te maken

--- ruimte_tba.py
+++ ruimte_tba.py
@@ -1,5 +1,5 @@
 #maak een loop dat eindigt wanneer je dood gaat of wanneer je het spel wint
 while True:
-    action = input("What ga je nu doen?(enter 'q' om te quitten)")
+    action = input("What ga je nu doen?(enter 'q' om te quitten): ")
     if action=='q':
         break
